c8776b9e681ea66b8eb3c2915750de3b987f4627
pyramidheadshark <notevengael@gmail.com>
2024-03-14 04:09:16 +0300

    Llama recovered (finally omg) + tags + req file + minor thingies

--- ML/summary_and_tag_selection/llama_summary_model.py
+++ ML/summary_and_tag_selection/llama_summary_model.py
@@ -1,12 +1,44 @@
 import requests
+import toml
 
 
-def ask_llama(context, prompt):
+def extract_tags_plus_prompt(file_path="llama_summary_tags.toml"):
+    with open(file_path, "r") as file:
+        data = toml.load(file)
+        tags = data["tags"]
+        prompt_parts = data["prompt_parts"]
+    return [tags, prompt_parts]
+
+
+def construct_list_of_tags(tags):  # Format: [tag, prompt, sub, subsub]
+    list_of_tags = []
+    shorten_list = []
+    for sub in tags:
+        for subsub in tags[sub]:
+            local_list = [tags[sub][subsub]["tag"], tags[sub][subsub]["prompt"], sub, subsub]
+            list_of_tags.append(local_list)
+            shorten_list.append(tags[sub][subsub]["tag"])
+    return [list_of_tags, shorten_list]
+
+
+def construct_prompt(prompt_parts, shorten_list, user_input):
+    prompt = prompt_parts["prompt_part_1"] + user_input + prompt_parts["prompt_part_2"] + " ['" + " ".join(
+        shorten_list).strip() + "']."
+    return prompt
+
+
+def search_by_tags(list_of_tags, response):
+    for tag in list_of_tags:
+        if tag[1] in response:
+            return tag
+
+
+def ask_llama(context, role="user"):
     invoke_url = "https://api.nvcf.nvidia.com/v2/nvcf/pexec/functions/0e349b44-440a-44e1-93e9-abe8dcb27158"
     fetch_url_format = "https://api.nvcf.nvidia.com/v2/nvcf/pexec/status/"
 
     headers = {
-        "Authorization": "Bearer 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",
+        "Authorization": "Bearer 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",
         "Accept": "application/json",
     }
 
@@ -14,7 +46,7 @@ def ask_llama(context, prompt):
         "messages": [
             {
                 "content": context,
-                "role": "user"
+                "role": role
             }
         ],
         "temperature": 0.2,
@@ -38,4 +70,12 @@ def ask_llama(context, prompt):
     return response.json()
 
 
-print(ask_llama("test", "test"))
+def llama_summary_model(user_input): # Format: [tag, prompt, sub, subsub]
+    tags, prompt_parts = extract_tags_plus_prompt()
+    list_of_tags, shorten_list = construct_list_of_tags(tags)
+    prompt = construct_prompt(prompt_parts, shorten_list, user_input)
+    response = ask_llama(prompt)
+    return search_by_tags(list_of_tags, response["choices"][0]["message"]["content"])
+
+
+# print(llama_summary_model("Где находится ближайшая кофейня?"))
