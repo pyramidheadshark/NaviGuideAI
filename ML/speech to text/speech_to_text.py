95397158e6aea3f7340da98dc421168e576ee297
Chaberis <kchaber@mail.ru>
2024-03-15 16:55:22 +0300

    stt

--- ML/speech to text/speech_to_text.py
+++ ML/speech to text/speech_to_text.py
@@ -1,10 +1,24 @@
 import os
 import whisper
+from pydub import AudioSegment
+from pydub.utils import which
 
+# Устанавливаем путь к ffmpeg, если он еще не установлен
+AudioSegment.converter = which("ffmpeg")
+
+def convert_to_mp3(input_audio, output_dir):
+    sound = AudioSegment.from_file(input_audio)
+    output_file = os.path.join(output_dir, "input_audio.mp3")
+    sound.export(output_file, format="mp3")
+    return output_file
 
 def process_and_save_audio(input_audio, output_dir):
-    model = whisper.load_model("tiny")
-    audio = whisper.load_audio(input_audio)
+    # Конвертируем входное аудио в mp3
+    mp3_audio = convert_to_mp3(input_audio, output_dir)
+
+    # Остальной код остается без изменений
+    model = whisper.load_model("small")
+    audio = whisper.load_audio(mp3_audio)
     audio = whisper.pad_or_trim(audio)
     mel = whisper.log_mel_spectrogram(audio).to(model.device)
     _, probs = model.detect_language(mel)
@@ -12,22 +26,11 @@ def process_and_save_audio(input_audio, output_dir):
     options = whisper.DecodingOptions()
     result = whisper.decode(model, mel, options)
     output_file = os.path.join(output_dir, f"output_{len(os.listdir(output_dir)) + 1}.txt")
-    with open(output_file, "w") as file:
+    with open(output_file, "w", encoding='utf-8') as file:
         file.write(result.text)
 
 
-# Usage example
-input_audio = "dictor.mp3"
+# Пример использования
+input_audio = "output_zh_1.wav"
 output_directory = "stt_output"
 process_and_save_audio(input_audio, output_directory)
-
-# import whisper
-# model = whisper.load_model("tiny")
-# audio = whisper.load_audio("dictor.mp3")
-# audio = whisper.pad_or_trim(audio)
-# mel = whisper.log_mel_spectrogram(audio).to(model.device)
-# _, probs = model.detect_language(mel)
-# print(f"Detected language: {max(probs, key=probs.get)}")
-# options = whisper.DecodingOptions()
-# result = whisper.decode(model, mel, options)
-# print(result.text)
