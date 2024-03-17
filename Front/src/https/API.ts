4d8bb1210225749cde6532878b4201142dda7d2a
pyramidheadshark <notevengael@gmail.com>
2024-03-17 11:59:17 +0300

    Reqs added

--- Front/src/https/API.ts
+++ Front/src/https/API.ts
@@ -1,9 +1,9 @@
-// import { $host } from ".";
+import { $host } from ".";
 
-// export const loginPost = async (login: string, pas: string) => {
-//   const response = await $host.post("api/user/login", { login, pas });
-//   return response;
-// };
+export const loginPost = async (login: string, pas: string) => {
+   const response = await $host.post("api/user/login", { login, pas });
+   return response;
+};
 
 // export const dbQueryPost = async ( query: string) => {
 //     const response = await $host.post('api/analyst/bdquery', { query })
